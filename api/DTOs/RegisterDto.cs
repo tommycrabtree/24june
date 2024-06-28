f3df7a360d9c51506f3753ff571f37b283d76067
Tommy <tommycrabtree@outlook.com>
2024-06-27 20:39:46 -0400

    End of Section 7

--- api/DTOs/RegisterDto.cs
+++ api/DTOs/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace api.DTOs
     {
         [Required]
         public string Username { get; set; }
+
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,5 @@
 using api.Extensions;
+using api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
